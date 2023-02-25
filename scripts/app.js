2f7912bae094ae8d5238efe49cbeb68a0d860080
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-02-25 01:26:20 -0800

    Added the Tetradic Palette

--- scripts/app.js
+++ scripts/app.js
@@ -61,31 +61,6 @@ class Alert extends Component {
   }
 }
 
-class ColorFormat extends Component {
-  constructor (parentId) {
-    super(parentId, false, false)
-    this.render()
-  }
-
-  render() {
-    const element = this.createComponent('div', 'selector')
-    element.innerHTML = `
-      <label for="color-space">
-        Choose A Color Format (default = HEX)
-      </label>
-      <br />
-      <select name="color" id="color-space">
-        <option>HEX</option>
-        <option>HSL</option>
-        <option>RGB</option>
-      </select>
-    `
-    document.body.append(element)
-
-  }
-}
-
-
 class ColorWheel extends Component {
 
   layersList = []
@@ -132,7 +107,7 @@ class ColorWheel extends Component {
       z 
     `)
     path.setAttribute('stroke', 'white');
-    path.setAttribute('fill', `hsl(${(360 / this.slices) * index} 100% ${10 + ((90 / this.layers) * layer)}%)`)
+    path.setAttribute('fill', `hsl(${(360 / this.slices) * index} 100% ${5 + ((95 / this.layers) * layer)}%)`)
     path.setAttribute('stroke-width', '0')
     path.style.transformOrigin = `${tOriginX}px ${tOriginY}px`
 
@@ -157,6 +132,41 @@ class ColorWheel extends Component {
           colorFunction = rgbString
       }
 
+      const tetradicDegrees = 40
+      const fluff = ['hsl','(', ')']
+      const originalHue = path.getAttribute('fill');
+      let hue = path.getAttribute('fill');
+      fluff.forEach(string => {hue = hue.replace(string, '')})
+      const hslArray = hue.split(' ')
+      const hueValue = hslArray[0]
+      const oppositeHue = Math.abs(hueValue - (360 / 2) < 0 ? 360 - Math.abs(hueValue - (360 / 2)) : (hueValue - (360 / 2)))
+      const thirdHue = Math.abs(oppositeHue - tetradicDegrees < 0 ? 360 - Math.abs(oppositeHue - tetradicDegrees) : oppositeHue - tetradicDegrees)
+      const fourthHue = Math.abs(oppositeHue + tetradicDegrees > 360 ? (oppositeHue + tetradicDegrees) - 360 : oppositeHue + tetradicDegrees)
+      const thirdHslString = `hsl(${thirdHue} ${hslArray[1]} ${hslArray[2]})`
+      const fourthHslString =  `hsl(${fourthHue} ${hslArray[1]} ${hslArray[2]})`
+      const oppositeHslString = `hsl(${oppositeHue} ${hslArray[1]} ${hslArray[2]})`
+      const hslStringArray = [
+        originalHue,
+        oppositeHslString,
+        thirdHslString,
+        fourthHslString
+      ]
+
+      const combinationColors = document.querySelectorAll('svg:has(>rect)')
+      combinationColors.forEach((color, i) => {
+        const rect = color.querySelector('rect')
+        const hexValueElement = color.previousSibling
+        hexValueElement.innerHTML = ColorHelper.CssHSLToHex(hslStringArray[i])
+        rect.setAttribute('fill', hslStringArray[i])
+      })
+
+
+      // const testDiv = document.createElement('div')
+      // testDiv.style.height = 20 + 'px';
+      // testDiv.style.width = 20 + 'px';
+      // testDiv.style.background = oppositeHslString;
+      //
+      // document.body.append(testDiv)
 
       navigator.clipboard.writeText(colorFunction)
         .then(r => {
@@ -295,6 +305,31 @@ class NavBar extends Component {
     this.render()
   }
 
+  renderSelector() {
+    const element = document.createElement('div')
+    element.id = 'select'
+    element.style.display = 'flex';
+    element.style.flexDirection = 'column'
+    element.style.justifyContent = 'center'
+    element.innerHTML = `
+      <h3>Color Format</h3>
+      <select name="color" id="color-space">
+        <option>HEX</option>
+        <option>HSL</option>
+        <option>RGB</option>
+      </select>
+    `
+    document.styleSheets[0].insertRule(`#nav #select h3 {
+      font-family: inherit;
+      background: #444444cc;
+      color: white;
+      padding: 0 .5rem;
+      border-radius: .25rem;  
+      margin-bottom: .25rem;    
+    }`,0)
+    return element
+  }
+
   renderBrand() {
     const img =document.createElement('img');
     img.src = "../../public/svg/Brand.svg";
@@ -311,6 +346,7 @@ class NavBar extends Component {
     ])
     const ss = document.styleSheets[0];
     const brand = this.renderBrand();
+    const selector = this.renderSelector()
     const toggle = new SideBarToggleButton(this.id).render()
     brand.style.filter = `drop-shadow(3px 2px 2px #00000099)`
 
@@ -324,11 +360,18 @@ class NavBar extends Component {
       background: #22222288;
       display: flex;
       justify-content: space-between;
+      align-items: center;
       margin: .25rem .25rem 0 .25rem;
       padding: .5rem;
       border-radius: .5rem;
     }`, 0 )
 
+    ss.insertRule(`#${this.id} select {
+      pointer-events: all;
+      text-align: center;
+      margin: auto;
+      cursor: pointer;
+    }`, 0)
 
     ss.insertRule(`#${this.id} button {
       pointer-events: all;
@@ -339,12 +382,14 @@ class NavBar extends Component {
       border-radius: .5rem;
       background: transparent;
       cursor: pointer;
+      margin-left: 50px;
     }`, 0)
     ss.insertRule(`#${this.id} button:hover {
       background: #33333388; 
     }`, 0)
 
     element.append(brand)
+    element.append(selector)
     element.append(toggle)
     outerDiv.append(element)
     document.body.append(outerDiv)
@@ -365,7 +410,6 @@ class SettingsBar extends Component {
       new ElementAttribute('id', this.id)
     ])
     const nav = document.getElementById('outer');
-    const selector = document.querySelector('.selector')
     const title = document.createElement('h2')
     title.innerText = 'Settings'
     aside.append(title)
@@ -377,7 +421,7 @@ class SettingsBar extends Component {
       const input = document.createElement('input')
       let timer
       label.htmlFor = name;
-      label.innerText = name.charAt(0).toUpperCase() + name.slice(1);
+      label.innerText = `${ name.charAt(0).toUpperCase() + name.slice(1)}: ${defaultValue}`;
       input.name = name;
       input.type = 'range';
       input.step = step;
@@ -387,10 +431,10 @@ class SettingsBar extends Component {
       input.id = name
 
       input.addEventListener('input', e => {
+        label.innerText = `${ name.charAt(0).toUpperCase() + name.slice(1)}: ${e.target.value}`;
         clearTimeout(timer)
         timer = setTimeout(() => {
           App.reInit()
-        console.log(e.target.value)
         },500)
       })
 
@@ -399,6 +443,7 @@ class SettingsBar extends Component {
     })
 
     ss.insertRule(`#${this.id} {
+      text-align: center;
       background: #33333399;
       position: fixed;
       border-radius: .5rem;
@@ -413,6 +458,8 @@ class SettingsBar extends Component {
     ss.insertRule(`.active {
       transform: translateX(0) !important;
     }`, 0 )
+
+
 
     ss.insertRule(`aside label {
       position: absolute;
@@ -436,50 +483,102 @@ class SettingsBar extends Component {
   }
 }
 
+class TetradicPalette extends Component{
+  constructor (parentId, id) {
+    super(parentId,false);
+    this.ss = document.styleSheets[0];
+    this.id = id;
+    this.render()
+  }
+
+  render() {
+    const element = this.createComponent('div', 'simple-palette', [
+      new ElementAttribute('id', this.id)
+    ])
+
+    this.ss.insertRule(`#${this.id} {
+      position: fixed;
+      inset: auto 0 0 0;
+      display: flex;
+      text-align: center;
+      justify-content: center;
+      gap: 1rem;
+      background: #555555ee;
+    }`, 0)
+
+    this.ss.insertRule(`#${this.id} h4 {
+      font-family: "Segoe UI", sans-serif;
+      font-weight: 700;
+    }`,0)
+
+    for (let i = 0; i < 4; i++ ){
+      const colorContainer = document.createElement('div')
+      const mainColor = this.createSVG(80, 30, `combination-color-${i}`)
+      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+      const hexString = document.createElement('h4')
+
+
+      hexString.innerText = `#ffffff`.toUpperCase()
+      rect.setAttribute('width', '80');
+      rect.setAttribute('height', '30');
+      rect.setAttribute('fill', 'white')
+      rect.setAttribute('rx', '10')
+      mainColor.setAttribute('fill', 'red')
+
+
+
+      mainColor.append(rect);
+      colorContainer.append(hexString)
+      colorContainer.append(mainColor);
+      element.append(colorContainer)
+    }
+  }
+}
 
 class App {
 
   static sliders = [
     {
       name: 'size',
-      defaultValue: 600,
+      defaultValue: 700,
       step: 100,
       maxValue: 1000,
       minValue: 100,
     },
     {
       name: 'gap',
-      defaultValue: 2,
+      defaultValue: 1,
       step: 1,
-      maxValue: 10,
+      maxValue: 5,
       minValue: 0,
     },
     {
       name: 'layers',
-      defaultValue: 9,
+      defaultValue: 20,
       step: 3,
       maxValue: 100,
       minValue: 3,
     },
     {
       name: 'slices',
-      defaultValue: 24,
+      defaultValue: 48,
       step: 3,
-      maxValue: 200,
+      maxValue: 360 / 2,
       minValue: 3,
     },
   ]
   static size = 700;
-  static gap = 2;
-  static layers = 9;
-  static slices = 24;
+  static gap = 1;
+  static layers = 20;
+  static slices = 48;
   static id = 'app'
   static colorWheelId = 'main'
   static init() {
     this.element = new ColorWheel('app', 'main', this.size, this.gap, this.layers, this.slices)
-    new ColorFormat()
+    // new ColorFormat()
     new NavBar('app', 'nav')
     new SettingsBar('app', 'settings', this.sliders )
+    new TetradicPalette('app','tetradic')
   }
 
 
@@ -488,7 +587,6 @@ class App {
     const gap = document.getElementById('gap').value;
     const layers = document.getElementById('layers').value;
     const slices = document.getElementById('slices').value;
-    console.log(this.element)
     this.element.rootElement.remove()
     this.element = new ColorWheel(this.id, this.colorWheelId, size, gap, layers, slices)
   }
